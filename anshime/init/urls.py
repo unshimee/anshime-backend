58f26e3dcbe8d5be89810a4678655a02d6021613
kde6260 <kde6260@gmail.com>
2020-10-19 22:59:45 +0900

    swagger 문서 제공하기 위해 django-rest-swagger 셋팅

--- anshime/init/urls.py
+++ anshime/init/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='언심이 API 문서')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-doc/', schema_view)
 ]
