441b05a5afc0a34a5f7ae3cbc3b86b7609f976d1
kde6260 <kde6260@gmail.com>
2020-10-21 23:17:32 +0900

    #2 유저의 귀가정보를 관리하는 모델 및 마이그레이션 생성

--- anshime/home/models.py
+++ anshime/home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import KakaoUser
+
+
+class RouteInfo(models.Model):
+    kakao_user_id = models.ForeignKey(KakaoUser, db_constraint=False, on_delete=models.DO_NOTHING)
+    depart_address = models.CharField(max_length=50, help_text="출발지 주소")
+    arrive_address = models.CharField(max_length=50, help_text="도착지 주소")
+    depart_at = models.DateTimeField(help_text="출발 시각")
+    arrive_at = models.DateTimeField(help_text="도착 시각")
+    transport = models.CharField(
+        help_text="이동 수단",
+        max_length=5,
+        choices=(("walk", "도보"), ("bus", "버스"), ("taxi", "택시"), ("own", "자가용"), ("other", "그 외"),),
+    )
+    finished_at = models.DateTimeField(help_text="귀가 종료 시각", null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "route_info"
+        verbose_name = "유저의 귀가 정보"
