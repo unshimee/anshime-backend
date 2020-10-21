986183cc0ae8848632c16d32e3e524bb5401843d
kde6260 <kde6260@gmail.com>
2020-10-21 23:00:32 +0900

    #2 카카오 유저 모델 및 마이그레이션 생성

--- anshime/authentication/models.py
+++ anshime/authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class KakaoUser(models.Model):
+    kakao_id = models.CharField(unique=True, max_length=30)
+    username = models.CharField(max_length=30)
+    email = models.EmailField(null=True)
+    gender = models.CharField(default="female", choices=(("female", "여성"),), max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "kakao_user"
+        verbose_name = "카카오 사용자 정보"
