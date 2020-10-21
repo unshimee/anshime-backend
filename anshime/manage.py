b080c1a725a1503282cea9c707d0d816277e008a
kde6260 <kde6260@gmail.com>
2020-10-21 23:24:06 +0900

    #2 registry를 로드하기 위해 setup() 호출

--- anshime/manage.py
+++ anshime/manage.py
@@ -3,9 +3,11 @@
 import os
 import sys
 
+import django
+
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'init.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "init.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +19,6 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    django.setup()
     main()
