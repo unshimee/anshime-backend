54dfc19f18c3f0327a5be7c48acfb88a9f8786cf
kde6260 <kde6260@gmail.com>
2020-10-23 19:32:21 +0900

    #6 테스트코드에서 url reversing을 위해 앱 이름 추가

--- anshime/init/urls.py
+++ anshime/init/urls.py
@@ -7,5 +7,5 @@ schema_view = get_swagger_view(title="언심이 API 문서")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-doc/", schema_view),
-    path("auth/", include("authentication.urls", namespace="auth")),
+    path("auth/", include(("authentication.urls", "authentication"), namespace="auth")),
 ]
