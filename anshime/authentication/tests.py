7deccc09485139944d2e921643165ff2d05c473b
kde6260 <kde6260@gmail.com>
2020-10-23 20:07:57 +0900

    #6 카카오 회원정보 필드가 유효한지 검사하도록 form 작성

--- anshime/authentication/tests.py
+++ anshime/authentication/tests.py
@@ -11,7 +11,7 @@ class TestKakaoSignup(TestCase):
 
     def test_kakao_signin_should_respond_created_if_user_login_with_kakao_first_time(self):
         # When: kakao_id가 1234인 유저가 카카오 로그인을 시도한다.
-        valid_request_body_data = {
+        valid_request_body = {
             'kakao_id': 1234,
             'username': 'DaEun',
             'email': 'kde6260@gmail.com',
@@ -19,7 +19,7 @@ class TestKakaoSignup(TestCase):
         }
         response = self.client.post(
             reverse('auth:kakao-signin'),
-            data=valid_request_body_data,
+            data=valid_request_body,
             content_type='application/json',
         )
 
@@ -30,11 +30,11 @@ class TestKakaoSignup(TestCase):
         })
 
         # Then: kakao_user 테이블에 row가 생성되어야 한다.
-        self.assertEqual(KakaoUser.objects.filter(**valid_request_body_data).count(), 1)
+        self.assertEqual(KakaoUser.objects.filter(**valid_request_body).count(), 1)
 
     def test_kakao_signin_should_respond_ok_if_user_login_with_kakao_again(self):
         # Given: kakao_id가 1234인 유저가 카카오 로그인을 시도한다.
-        first_valid_request_body_data = {
+        first_valid_request_body = {
             'kakao_id': 1234,
             'username': 'DaEun',
             'email': 'kde6260@gmail.com',
@@ -42,12 +42,12 @@ class TestKakaoSignup(TestCase):
         }
         self.client.post(
             reverse('auth:kakao-signin'),
-            data=first_valid_request_body_data,
+            data=first_valid_request_body,
             content_type='application/json',
         )
 
         # When: kakao_id가 1234인 유저가 수정된 정보로 카카오 로그인을 다시 시도한다.
-        second_valid_request_body_data = {
+        second_valid_request_body = {
             'kakao_id': 1234,
             'username': 'Jadu',
             'email': 'jadu@gmail.com',
@@ -55,7 +55,7 @@ class TestKakaoSignup(TestCase):
         }
         response = self.client.post(
             reverse('auth:kakao-signin'),
-            data=second_valid_request_body_data,
+            data=second_valid_request_body,
             content_type='application/json',
         )
 
@@ -67,5 +67,97 @@ class TestKakaoSignup(TestCase):
 
         # Then: kakao_user 테이블에 row가 수정된 필드로 1개개만 존재해야 한다.
         kakao_user = KakaoUser.objects.get(kakao_id=1234)
-        self.assertEqual(kakao_user.email, second_valid_request_body_data['email'])
-        self.assertEqual(kakao_user.username, second_valid_request_body_data['username'])
+        self.assertEqual(kakao_user.email, second_valid_request_body['email'])
+        self.assertEqual(kakao_user.username, second_valid_request_body['username'])
+
+    def test_kakao_signin_should_respond_bad_request_if_kakao_id_is_omitted(self):
+        # Given: kakao_id가 없다.
+        request_body_without_kakao_id = {
+            'username': 'DaEun',
+            'email': 'kde6260@gmail.com',
+            'gender': 'female',
+        }
+
+        response = self.client.post(
+            reverse('auth:kakao-signin'),
+            data=request_body_without_kakao_id,
+            content_type='application/json',
+        )
+
+        # Then: 에러메세지와 함께 상태코드 400로 응답해야 한다.
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(json.loads(response.content.decode()), {
+            'errors': ['카카오 ID가 유효하지 않습니다.'],
+        })
+
+        # Then: kakao_user 테이블에 row가 없어야 한다.
+        self.assertFalse(KakaoUser.objects.filter(**request_body_without_kakao_id))
+
+    def test_kakao_signin_should_respond_bad_request_if_email_is_omitted(self):
+        # Given: 이메일이 없다.
+        request_body_without_email = {
+            'kakao_id': 1234,
+            'username': 'DaEun',
+            'gender': 'female',
+        }
+
+        response = self.client.post(
+            reverse('auth:kakao-signin'),
+            data=request_body_without_email,
+            content_type='application/json',
+        )
+
+        # Then: 에러메세지와 함께 상태코드 400로 응답해야 한다.
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(json.loads(response.content.decode()), {
+            'errors': ['이메일이 유효하지 않습니다.'],
+        })
+
+        # Then: kakao_user 테이블에 row가 없어야 한다.
+        self.assertFalse(KakaoUser.objects.filter(**request_body_without_email))
+
+    def test_kakao_signin_should_respond_bad_request_if_username_is_omitted(self):
+        # Given: 프로필 이름이 없다.
+        request_body_without_username = {
+            'kakao_id': 1234,
+            'email': 'kde6260@gmail.com',
+            'gender': 'female',
+        }
+
+        response = self.client.post(
+            reverse('auth:kakao-signin'),
+            data=request_body_without_username,
+            content_type='application/json',
+        )
+
+        # Then: 에러메세지와 함께 상태코드 400로 응답해야 한다.
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(json.loads(response.content.decode()), {
+            'errors': ['프로필 이름이 유효하지 않습니다.'],
+        })
+
+        # Then: kakao_user 테이블에 row가 없어야 한다.
+        self.assertFalse(KakaoUser.objects.filter(**request_body_without_username))
+
+    def test_kakao_signin_should_respond_bad_request_if_gender_is_omitted(self):
+        # Given: 성별이 없다.
+        request_body_without_gender = {
+            'kakao_id': 1234,
+            'username': 'DaEun',
+            'email': 'kde6260@gmail.com',
+        }
+
+        response = self.client.post(
+            reverse('auth:kakao-signin'),
+            data=request_body_without_gender,
+            content_type='application/json',
+        )
+
+        # Then: 에러메세지와 함께 상태코드 400로 응답해야 한다.
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(json.loads(response.content.decode()), {
+            'errors': ['성별이 유효하지 않습니다.'],
+        })
+
+        # Then: kakao_user 테이블에 row가 없어야 한다.
+        self.assertFalse(KakaoUser.objects.filter(**request_body_without_gender))
--- anshime/authentication/views.py
+++ anshime/authentication/views.py
@@ -4,12 +4,19 @@ from http import HTTPStatus
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 
+from authentication.forms import KakaoUserForm
 from authentication.models import KakaoUser
 
 
 @require_POST
 def signin_with_kakao(request):
     user_info = json.loads(request.body.decode())
+
+    form = KakaoUserForm(user_info)
+    if not form.is_valid():
+        return JsonResponse({
+            'errors': form.errors['__all__']
+        }, status=HTTPStatus.BAD_REQUEST)
 
     user, created = KakaoUser.objects.update_or_create(
         kakao_id=user_info['kakao_id'],
