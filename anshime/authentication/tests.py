b3413ca34b487731872371ec8f1fb31a9c1dfd5a
kde6260 <kde6260@gmail.com>
2020-10-23 19:33:41 +0900

    #6 카카오 회원정보를 생성하도록 API 작성

--- anshime/authentication/tests.py
+++ anshime/authentication/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+import json
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from authentication.models import KakaoUser
+
+
+class TestKakaoSignup(TestCase):
+
+    def test_kakao_signin_should_respond_created_if_user_login_with_kakao_first_time(self):
+        # When: kakao_id가 1234인 유저가 카카오 로그인을 시도한다.
+        valid_request_body_data = {
+            'kakao_id': 1234,
+            'username': 'DaEun',
+            'email': 'kde6260@gmail.com',
+            'gender': 'female',
+        }
+        response = self.client.post(
+            reverse('auth:kakao-signin'),
+            data=valid_request_body_data,
+            content_type='application/json',
+        )
+
+        # Then: 에러없이 상태코드 201로 응답해야 한다.
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertEqual(json.loads(response.content.decode()), {
+            'errors': [],
+        })
+
+        # Then: kakao_user 테이블에 row가 생성되어야 한다.
+        self.assertEqual(KakaoUser.objects.filter(**valid_request_body_data).count(), 1)
+
+    def test_kakao_signin_should_respond_ok_if_user_login_with_kakao_again(self):
+        # Given: kakao_id가 1234인 유저가 카카오 로그인을 시도한다.
+        first_valid_request_body_data = {
+            'kakao_id': 1234,
+            'username': 'DaEun',
+            'email': 'kde6260@gmail.com',
+            'gender': 'female',
+        }
+        self.client.post(
+            reverse('auth:kakao-signin'),
+            data=first_valid_request_body_data,
+            content_type='application/json',
+        )
+
+        # When: kakao_id가 1234인 유저가 수정된 정보로 카카오 로그인을 다시 시도한다.
+        second_valid_request_body_data = {
+            'kakao_id': 1234,
+            'username': 'Jadu',
+            'email': 'jadu@gmail.com',
+            'gender': 'female',
+        }
+        response = self.client.post(
+            reverse('auth:kakao-signin'),
+            data=second_valid_request_body_data,
+            content_type='application/json',
+        )
+
+        # Then: 에러없이 상태코드 200으로 응답해야 한다.
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(json.loads(response.content.decode()), {
+            'errors': [],
+        })
+
+        # Then: kakao_user 테이블에 row가 수정된 필드로 1개개만 존재해야 한다.
+        kakao_user = KakaoUser.objects.get(kakao_id=1234)
+        self.assertEqual(kakao_user.email, second_valid_request_body_data['email'])
+        self.assertEqual(kakao_user.username, second_valid_request_body_data['username'])
--- anshime/authentication/views.py
+++ anshime/authentication/views.py
@@ -1,2 +1,24 @@
-def signup_with_kakao(request):
-    pass
+import json
+from http import HTTPStatus
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+from authentication.models import KakaoUser
+
+
+@require_POST
+def signin_with_kakao(request):
+    user_info = json.loads(request.body.decode())
+
+    user, created = KakaoUser.objects.update_or_create(
+        kakao_id=user_info['kakao_id'],
+        defaults={
+            'email': user_info['email'],
+            'username': user_info['username']
+        }
+    )
+
+    if created:
+        return JsonResponse({'errors': []}, status=HTTPStatus.CREATED)
+    return JsonResponse({'errors': []}, status=HTTPStatus.OK)
