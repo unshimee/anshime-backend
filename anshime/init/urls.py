d8ef287b0911acbb3b3fc028aa15d1e6ed603518
kde6260 <kde6260@gmail.com>
2020-10-24 16:52:38 +0900

    #6 drf-yasg로 swagger 문서 제공하도록 설정

--- anshime/init/urls.py
+++ anshime/init/urls.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
-schema_view = get_swagger_view(title="언심이 API 문서")
+
+schema_view = get_schema_view(openapi.Info(
+    title='언심이 API 문서',
+    default_version='v1',
+    description='backend API specification',
+    contact=openapi.Contact('kde6260@gmail.com'),
+),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-doc/", schema_view),
+    path("api-doc/", schema_view.with_ui('swagger')),
     path("auth/", include(("authentication.urls", "authentication"), namespace="auth")),
 ]
