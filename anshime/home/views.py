8c4fdab72c7d91b96d31146a8c11d11f34703383
kde6260 <kde6260@gmail.com>
2020-10-26 23:15:39 +0900

    #9 swaggger 문서에 API 스펙 추가

--- anshime/home/views.py
+++ anshime/home/views.py
@@ -1,10 +1,39 @@
 from django.http import JsonResponse
+from drf_yasg import openapi as oa
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from home.helpers import request_coord_to_region_code
 
 NUMBER_OF_PARSED_SPECIAL_CITY = 3
 
 
+class CoordinateSerializer(serializers.Serializer):
+    x = serializers.CharField(max_length=200)
+    y = serializers.CharField(max_length=200)
+
+
+@swagger_auto_schema(
+    operation_description='GET /home/convert-coord-to-address/',
+    method='get',
+    query_serializer=CoordinateSerializer,
+    responses={
+        '200': oa.Response(
+            description='주소 요청에 성공했을 때 response',
+            schema=oa.Schema(type=oa.TYPE_OBJECT, properties={
+                '1depth': oa.Schema(type=oa.TYPE_STRING, description='최상위 지역명(시/군/면)'),
+                '2depth': oa.Schema(type=oa.TYPE_STRING, description='차상위 지역명(구/면)'),
+                '3depth': oa.Schema(type=oa.TYPE_STRING, description='최하위 지역명(동/읍/리'),
+            })
+        ),
+        '400': oa.Response(
+            description='좌표가 유효하지 않을 때 response',
+            schema=oa.Schema(type=oa.TYPE_OBJECT, properties={
+                'errors': oa.Schema(type=oa.TYPE_ARRAY, description='에러 리스트', items=oa.Schema(type=oa.TYPE_STRING)),
+            })
+        ),
+    }
+)
 @api_view(['get'])
 def convert_coordinate_to_address(request):
     x = request.GET.get('x')
